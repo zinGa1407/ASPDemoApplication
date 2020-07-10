42820e0c349a465865dabb044db236dd53d40d52
Alexander <alexanderelzinga94@gmail.com>
2020-07-10 19:01:35 +0200

    Page with List

---
+++ ASPDemoApplication/MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "Alex", LastName = "Elzinga", Age = 25 });
+            people.Add(new PersonModel { FirstName = "Bobby", LastName = "Elzinga", Age = 49 });
+            people.Add(new PersonModel { FirstName = "John", LastName = "Doe", Age = 36 });
+
+            return View(people);
+        }
+    }
+}
