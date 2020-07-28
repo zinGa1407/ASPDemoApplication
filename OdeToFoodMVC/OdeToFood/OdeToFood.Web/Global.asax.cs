a4582a60883bad3ee39a3b3f01598643893a3597
Alexander <alexanderelzinga94@gmail.com>
2020-07-28 15:38:45 +0200

    Section 4 completed

--- OdeToFoodMVC/OdeToFood/OdeToFood.Web/Global.asax.cs
+++ OdeToFoodMVC/OdeToFood/OdeToFood.Web/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace OdeToFood.Web
 {
@@ -12,11 +13,12 @@ namespace OdeToFood.Web
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            ContainerConfig.RegisterContainer();
+            ContainerConfig.RegisterContainer(GlobalConfiguration.Configuration);
         }
     }
 }
