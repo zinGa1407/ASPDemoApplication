fc1127e6f8138b8c704c7c35a7d7e080f7862df6
Alexander <alexanderelzinga94@gmail.com>
2020-07-30 11:36:45 +0200

    Section 7 complete.

--- OdeToFoodMVC/OdeToFood/OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFoodMVC/OdeToFood/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -67,6 +67,7 @@ namespace OdeToFood.Web.Controllers
             if (ModelState.IsValid)
             {
                 db.Edit(restaurant);
+                TempData["Message"] = "You have saved the restaurant!";
                 return RedirectToAction("Details", new { id = restaurant.Id });
             }
             return View(restaurant);
